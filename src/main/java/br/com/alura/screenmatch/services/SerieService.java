0b08f9ecd800fa6fb0f69523d9243b48f266189e
Joao Pedro Beraldo <jpberaldo98@hotmail.com>
2024-05-31 14:27:29 -0300

    Implementação de detalhamento de série por Id

--- src/main/java/br/com/alura/screenmatch/services/SerieService.java
+++ src/main/java/br/com/alura/screenmatch/services/SerieService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -27,11 +28,24 @@ public class SerieService {
         return converteDados(repositorio.ordernarLancamentosPorData());
     }
 
+    public SerieDTO buscarPorId(Long id) {
+
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()) {
+
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(),
+                    s.getPoster(), s.getSinopse());
+
+        } else {
+            return null;
+        }
+    }
+
     private List<SerieDTO> converteDados(List<Serie> series) {
         return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(),
                         s.getPoster(), s.getSinopse())).collect(Collectors.toList());
     }
-
 
 }
